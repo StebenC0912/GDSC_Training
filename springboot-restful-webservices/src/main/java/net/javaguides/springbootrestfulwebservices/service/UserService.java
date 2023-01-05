a3abd6a9629b288fc62e9b74a2d099bcb244cb4f
StebenC0912 <stebenc09122002@gmail.com>
2023-01-05 23:29:10 +0700

    Initial commit

---
+++ springboot-restful-webservices/src/main/java/net/javaguides/springbootrestfulwebservices/service/UserService.java
@@ -0,0 +1,12 @@
+package net.javaguides.springbootrestfulwebservices.service;
+import net.javaguides.springbootrestfulwebservices.entity.User;
+
+import java.util.List;
+
+public interface UserService {
+    User createUser (User user);
+    User getUserById(Long userId);
+    List<User> getAllUser();
+    User updateUser(User user);
+    void deleteUser(Long userId);
+}
---
+++ springboot-restful-webservices/src/main/java/net/javaguides/springbootrestfulwebservices/service/impl/UserServiceImpl.java
@@ -0,0 +1,46 @@
+package net.javaguides.springbootrestfulwebservices.service.impl;
+
+import lombok.AllArgsConstructor;
+import net.javaguides.springbootrestfulwebservices.entity.User;
+import net.javaguides.springbootrestfulwebservices.respository.UserRepository;
+import net.javaguides.springbootrestfulwebservices.service.UserService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class UserServiceImpl implements UserService {
+    private UserRepository userRepository;
+    @Override
+    public User createUser(User user){
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User getUserById(Long userId) {
+        Optional<User> optionalUser = userRepository.findById(userId);
+        return optionalUser.get();
+    }
+
+    @Override
+    public List<User> getAllUser() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User updateUser(User user) {
+        User existUser = userRepository.findById(user.getId()).get();
+        existUser.setFirstName(user.getFirstName());
+        existUser.setLastName(user.getLastName());
+        existUser.setEmail(user.getEmail());
+        User updateUser = userRepository.save(existUser);
+        return updateUser;
+    }
+
+    @Override
+    public void deleteUser(Long userId) {
+        userRepository.deleteById(userId);
+    }
+}
