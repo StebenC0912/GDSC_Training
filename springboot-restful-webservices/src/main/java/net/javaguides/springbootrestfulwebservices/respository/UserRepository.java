a61bb453f37a882841b639861464cf872fca2e4f
StebenC0912 <stebenc09122002@gmail.com>
2023-02-22 20:48:54 +0700

    delete old project

--- springboot-restful-webservices/src/main/java/net/javaguides/springbootrestfulwebservices/respository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package net.javaguides.springbootrestfulwebservices.respository;
-
-import net.javaguides.springbootrestfulwebservices.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
