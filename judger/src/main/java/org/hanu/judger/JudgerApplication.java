0afb159fc31cae91633e703cce0045918e605f38
StebenC0912 <stebenc09122002@gmail.com>
2023-03-03 21:27:04 +0700

    commit

---
+++ judger/src/main/java/org/hanu/judger/JudgerApplication.java
@@ -0,0 +1,13 @@
+package org.hanu.judger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JudgerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JudgerApplication.class, args);
+	}
+
+}
---
+++ judger/src/test/java/org/hanu/judger/JudgerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hanu.judger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JudgerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
